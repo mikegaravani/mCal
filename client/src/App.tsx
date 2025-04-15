207a4948a0582748c95961553cb086414be3755a
Mike Garavani <scotchmagico@gmail.com>
2025-04-16 01:11:37 +0200

    add RequireAuth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RequireAuth from "./components/Login/RequireAuth";
 import Layout from "./components/Layout/Layout";
 import HomePage from "./components/HomePage/HomePage";
 import Calendar from "./components/Calendar/Calendar";
@@ -13,15 +14,21 @@ function App() {
     <>
       <Router>
         <Routes>
-          {/* ROUTES WITH SIDEBAR LAYOUT */}
-          <Route element={<Layout />}>
+          {/* Protected routes */}
+          <Route
+            element={
+              <RequireAuth>
+                <Layout />
+              </RequireAuth>
+            }
+          >
             <Route path="/" element={<HomePage />} />
             <Route path="/pomodoro" element={<Pomodoro />} />
             <Route path="/calendar" element={<Calendar />} />
             <Route path="/notes" element={<Notes />} />
           </Route>
 
-          {/* ROUTES WITHOUT SIDEBAR LAYOUT */}
+          {/* Public routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
---
+++ client/src/components/Login/RequireAuth.tsx
@@ -0,0 +1,18 @@
+import { Navigate } from "react-router-dom";
+import type { ReactNode } from "react";
+
+type RequireAuthProps = {
+  children: ReactNode;
+};
+
+const RequireAuth = ({ children }: RequireAuthProps) => {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default RequireAuth;
