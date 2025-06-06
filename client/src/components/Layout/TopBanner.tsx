82bb3c340f0702afe20a143612227385d5481134
Mike Garavani <scotchmagico@gmail.com>
2025-06-06 21:21:23 +0200

    feat: top banner tweak button to go home

--- client/src/components/Layout/TopBanner.tsx
+++ client/src/components/Layout/TopBanner.tsx
@@ -1,8 +1,10 @@
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { MenuIcon } from "lucide-react";
 import TimeMachine from "@/components/TimeMachine/TimeMachine";
+import { useNavigate } from "react-router-dom";
 
 export function TopBanner() {
+  const navigate = useNavigate();
   return (
     <header className="fixed top-0 left-0 right-0 z-50 flex h-14 items-center border-b px-4 lg:px-6 bg-background">
       <div className="flex w-full items-center justify-between">
@@ -11,7 +13,15 @@ export function TopBanner() {
             <MenuIcon className="h-6 w-6" />
             <span className="sr-only">Toggle Sidebar</span>
           </SidebarTrigger>
-          <h2 className="text-lg font-semibold">mCal</h2>
+          <button
+            onClick={() => {
+              navigate("/");
+            }}
+            title="Go to homepage"
+            className="text-lg font-semibold bg-transparent border-none p-0 m-0 cursor-pointer hover:underline"
+          >
+            mCal
+          </button>
         </div>
 
         <div className="flex items-center gap-4">
